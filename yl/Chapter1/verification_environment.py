d22a073499fdc7ed55facfff1d806d1125c28938
li <yangli@gennlife.com>
2023-06-04 21:53:50 +0800

    update chapter2

--- yl/Chapter1/verification_environment.py
+++ yl/Chapter1/verification_environment.py
@@ -1,6 +1,7 @@
-# Example of configuring environment
 import torch
 import pytorch_lightning as pl
+
+# Example of configuring environment
 
 print("torch version:", torch.__version__)
 print("pytorch lightning version:", pl.__version__)
--- yl/Chapter2/Perceptron_model_XOR.py
+++ yl/Chapter2/Perceptron_model_XOR.py
@@ -10,14 +10,14 @@ print("torch version:", torch.__version__)
 print("pytorch ligthening version:", pl.__version__)
 
 xor_input = [Variable(torch.Tensor([0, 0])),
-           Variable(torch.Tensor([0, 1])),
-           Variable(torch.Tensor([1, 0])),
-           Variable(torch.Tensor([1, 1]))]
+             Variable(torch.Tensor([0, 1])),
+             Variable(torch.Tensor([1, 0])),
+             Variable(torch.Tensor([1, 1]))]
 
 xor_target = [Variable(torch.Tensor([0])),
-           Variable(torch.Tensor([1])),
-           Variable(torch.Tensor([1])),
-           Variable(torch.Tensor([0]))]
+              Variable(torch.Tensor([1])),
+              Variable(torch.Tensor([1])),
+              Variable(torch.Tensor([0]))]
 
 xor_data = list(zip(xor_input, xor_target))
 train_loader = DataLoader(xor_data, batch_size=1000)
@@ -26,12 +26,9 @@ train_loader = DataLoader(xor_data, batch_size=1000)
 class XORModel(pl.LightningModule):
     def __init__(self):
         super(XORModel, self).__init__()
-
         self.input_layer = nn.Linear(2, 4)
         self.output_layer = nn.Linear(4, 1)
-
         self.sigmoid = nn.Sigmoid()
-
         self.loss = nn.MSELoss()
 
     def forward(self, input):
@@ -70,19 +67,18 @@ trainer.fit(model, train_dataloaders=train_loader)
 
 print(checkpoint_callback.best_model_path)
 train_model = model.load_from_checkpoint(checkpoint_callback.best_model_path)
-#test = torch.utils.data.DataLoader(xor_input, batch_size=1)
+# test = torch.utils.data.DataLoader(xor_input, batch_size=1)
 for val in xor_input:
-  _ = train_model(val)
-  print([int(val[0]),int(val[1])], int(_.round()))
+    _ = train_model(val)
+    print([int(val[0]), int(val[1])], int(_.round()))
 
-
-#7.accuracy 准确率  稳定性
+# 7.accuracy 准确率  稳定性
 total_accuracy = []
 for xor_input, xor_target in train_loader:
-  for i in range(100):
-    output_tensor = train_model(xor_input)
-    #https://torchmetrics.readthedocs.io/en/stable/classification/accuracy.html#multiclassaccuracy
-    test_accuracy = accuracy(preds=output_tensor, target=xor_target,task="binary")
-    total_accuracy.append(test_accuracy)
+    for i in range(100):
+        output_tensor = train_model(xor_input)
+        # https://torchmetrics.readthedocs.io/en/stable/classification/accuracy.html#multiclassaccuracy
+        test_accuracy = accuracy(preds=output_tensor, target=xor_target, task="binary")
+        total_accuracy.append(test_accuracy)
 total_accuracy = torch.mean(torch.stack(total_accuracy))
 print("TOTAL ACCURACY FOR 100 ITERATIONS: ", total_accuracy.item())
